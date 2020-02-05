4bd4397ee999dd571a19ca49545716e56dc02939
zhouwentong <zhouwentong1993@gmail.com>
2020-02-05 22:51:42 +0800

    增加了 InFlightRequests 超时机制

--- rpc-netty/src/main/java/com/wentong/rpc/netty/transport/InFlightRequests.java
+++ rpc-netty/src/main/java/com/wentong/rpc/netty/transport/InFlightRequests.java
@@ -4,9 +4,7 @@ import java.io.Closeable;
 import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
-import java.util.concurrent.Semaphore;
-import java.util.concurrent.TimeUnit;
-import java.util.concurrent.TimeoutException;
+import java.util.concurrent.*;
 
 /**
  * 正在进行中，还没有结束的请求们
@@ -19,6 +17,13 @@ public class InFlightRequests implements Closeable {
     // 最多只允许放 10 个请求
     private Semaphore semaphore = new Semaphore(10);
 
+    private final ScheduledExecutorService scheduledExecutorService = Executors.newSingleThreadScheduledExecutor();
+    private final ScheduledFuture scheduledFuture;
+
+    public InFlightRequests() {
+        scheduledFuture = scheduledExecutorService.scheduleAtFixedRate(this::removeTimeout, 0, 10, TimeUnit.SECONDS);
+    }
+
     // 往 futureMap 里放，如果超时，抛出 TimeoutException
     public void put(ResponseFuture responseFuture) throws TimeoutException, InterruptedException {
         if (semaphore.tryAcquire(3, TimeUnit.SECONDS)) {
@@ -29,12 +34,30 @@ public class InFlightRequests implements Closeable {
     }
 
     public void remove(ResponseFuture responseFuture) {
-        futureMap.remove(responseFuture.getRequestId());
+        ResponseFuture remove = futureMap.remove(responseFuture.getRequestId());
+        // 释放信号量
+        if (remove != null) {
+            semaphore.release();
+        }
+    }
+
+    private void removeTimeout() {
+        long nowTimeOfNow = System.nanoTime();
+        futureMap.entrySet().removeIf(entry -> {
+            // 如果里面的请求耗时超过了 10s，则移除该请求，并释放一个信号量
+            if (nowTimeOfNow - entry.getValue().getTimestamp() > TimeUnit.SECONDS.toNanos(10)) {
+                semaphore.release();
+                return true;
+            } else {
+                return false;
+            }
+        });
     }
 
 
     @Override
     public void close() throws IOException {
-
+        scheduledExecutorService.shutdown();
+        scheduledFuture.cancel(true);
     }
 }
